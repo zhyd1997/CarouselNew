03b9a6e3506737eb2cc9a928eedd738e26cdd46a
007tom <zhyd007@gmail.com>
2020-04-17 19:25:57 +0800

    消除魔法数字，fixed bug

--- index.js
+++ index.js
@@ -5,35 +5,37 @@ let elIndicatorList = document.querySelector('.indicator-list');
 let elIndicatorItem = document.querySelectorAll('.indicator-item');
 
 let currentIndex = 0;
+const LIST_LENGTH = elListCarouselItem.length;
+console.log(LIST_LENGTH);
 
 elArrowRight.onclick = function() {
     let oldIndex = currentIndex;
 
-    if (currentIndex >= 3) {
+    if (currentIndex >= LIST_LENGTH - 1) {
         currentIndex = 0;
     } else {
         currentIndex = currentIndex + 1;
     }
 
-    elIndicatorItem[oldIndex].classList.remove('indicator-item_current');
-    elIndicatorItem[currentIndex].classList.add('indicator-item_current');
     elListCarouselItem[oldIndex].classList.remove('carousel-item_current');
     elListCarouselItem[currentIndex].classList.add('carousel-item_current');
+    elIndicatorItem[oldIndex].classList.remove('indicator-item_current');
+    elIndicatorItem[currentIndex].classList.add('indicator-item_current');
 }
 
 elArrowLeft.onclick = function() {
     let oldIndex = currentIndex;
 
     if (currentIndex <= 0) {
-        currentIndex = 3;
+        currentIndex = LIST_LENGTH - 1;
     } else {
         currentIndex = currentIndex - 1;
     }
 
-    elIndicatorItem[oldIndex].classList.remove('indicator-item_current');
-    elIndicatorItem[currentIndex].classList.add('indicator-item_current');
     elListCarouselItem[oldIndex].classList.remove('carousel-item_current');
     elListCarouselItem[currentIndex].classList.add('carousel-item_current');
+    elIndicatorItem[oldIndex].classList.remove('indicator-item_current');
+    elIndicatorItem[currentIndex].classList.add('indicator-item_current');
 }
 
 elIndicatorList.onclick = function(event) {
@@ -46,8 +48,10 @@ elIndicatorList.onclick = function(event) {
     let targetIndex = parseInt(target.dataset.index, 10);
     let oldIndex = currentIndex;
 
-    elIndicatorItem[oldIndex].classList.remove('indicator-item_current');
-    elIndicatorItem[targetIndex].classList.add('indicator-item_current');
     elListCarouselItem[oldIndex].classList.remove('carousel-item_current');
     elListCarouselItem[targetIndex].classList.add('carousel-item_current');
+    elIndicatorItem[oldIndex].classList.remove('indicator-item_current');
+    elIndicatorItem[targetIndex].classList.add('indicator-item_current');
+
+    currentIndex = targetIndex;
 }
